724acab3ac3191dd3665563406fe9d4edda39a7b
unknown <huhmlbb@gmail.com>
2024-10-26 00:49:29 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+var question1 = ("HTML nima va u nimadan iborat? - ")
+var question2 = ("HTML hujjatini yaratish uchun qaysi teg bilan boshlash kerak? - ")
+var question3 = ("HTMLda matnni qalin qilish uchun qaysi tegdan foydalanamiz? - ")
+var question4 = ("<a> tegining vazifasi nima va qanday qilib havola yaratamiz? - ")
+var question5 = ("Rasm qo'shish uchun qaysi tegdan foydalanamiz? - ")
+
+var question6 = ("<ul> va <ol> teglarining farqi nima? - ")
+var question7 = ("CSS nima va u nima uchun ishlatiladi? - ")
+var question8 = ("CSSda rang berish uchun qanday xususiyatlardan foydalanamiz? - ")
+var question9 = ("margin va padding o'rtasidagi farq nima? - ")
+var question10 = ("CSSda matnni markazlashtirish uchun qaysi xususiyatdan foydalanamiz? - ")
+
+var question11 = ("CSS faylini HTMLga qanday ulash mumkin? - ")
+var question12 = ("JavaScript nima va u HTML bilan qanday ishlaydi? - ")
+var question13 = ("JavaScriptda o'zgaruvchilar qanday e'lon qilinadi? - ")
+var question14 = ("alert() funksiyasi nima uchun ishlatiladi? - ")
+var question15 = ("HTML elementini JavaScript orqali qanday tanlash mumkin? - ")
+
+var answer1 = prompt(question1)
+var answer2 = prompt(question2)
+var answer3 = prompt(question3)
+var answer4 = prompt(question4)
+var answer5 = prompt(question5)
+
+var answer6 = prompt(question6)
+var answer7 = prompt(question7)
+var answer8 = prompt(question8)
+var answer9 = prompt(question9)
+var answer10 = prompt(question10)
+
+var answer11 = prompt(question11)
+var answer12 = prompt(question12)
+var answer13 = prompt(question13)
+var answer14 = prompt(question14)
+var answer15 = prompt(question15)
+
+var a1 = ("HTML - veb-sahifa tuzilishini yaratadigan til bo‘lib, uning asosiy qismlari teglar va elementlardan iborat.")
+var a2 = ("HTML hujjati <!DOCTYPE html> bilan boshlanadi.")
+var a3 = ("Matnni qalin qilish uchun <b> yoki <strong> teglari ishlatiladi.")
+var a4 = ("<a> tegi havola yaratadi, masalan:</a>.")
+var a5 = ("Rasm qo'shish uchun <img> tegi ishlatiladi")
+
+var a6 = ("<ul> tartibsiz (belgili) ro'yxat yaratadi, <ol> esa tartibli (raqamli) ro'yxat yaratadi.")
+var a7= ("CSS - veb-sahifa uslubini yaratish uchun ishlatiladigan til; u sahifaning ko‘rinishi va dizaynini boshqaradi.")
+var a8 = ("color matnning rangini, background-color esa elementning fon rangini o'zgartiradi.")
+var a9 = ("margin elementning tashqi bo'sh joyini, padding esa ichki bo'sh joyini belgilaydi.")
+var a10 = ("Matnni markazlashtirish uchun text-align: center; xususiyati ishlatiladi.")
+
+var a11 = ("<link> tegidan foydalanib ulash mumkin.")
+var a12 = ("JavaScript - veb-sahifaga interaktivlik qo'shish uchun ishlatiladigan dasturlash tili bo‘lib, HTML bilan <script> tegida birikadi.")
+var a13 = ("var, let, yoki const kalit so'zlari bilan o'zgaruvchilar e'lon qilinadi, masalan, let x = 5;.")
+var a14 = ("alert() foydalanuvchiga xabar chiqarish uchun ishlatiladi.")
+var a15 = ("document.getElementById  yoki document.querySelector  kabi metodlardan foydalaniladi.")
+
+
+console.log(question1,answer1,a1)
+console.log(question2,answer2,a2)
+console.log(question3,answer3,a3)
+console.log(question4,answer4,a4)
+console.log(question5,answer5,a5)
+
+
+console.log(question6,answer6,a6)
+console.log(question7,answer7,a7)
+console.log(question8,answer8,a8)
+console.log(question9,answer9,a9)
+console.log(question10,answer10,a10)
+
+console.log(question11,answer11,a11)
+console.log(question12,answer12,a12)
+console.log(question13,answer13,a13)
+console.log(question14,answer14,a14)
+console.log(question15,answer15,a15)
+
