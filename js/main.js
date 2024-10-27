721490adc86b20bfc7ce3901ccda31c31b337476
unknown <huhmlbb@gmail.com>
2024-10-27 14:10:45 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,74 +1,255 @@
-var question1 = ("HTML nima va u nimadan iborat? - ")
-var question2 = ("HTML hujjatini yaratish uchun qaysi teg bilan boshlash kerak? - ")
-var question3 = ("HTMLda matnni qalin qilish uchun qaysi tegdan foydalanamiz? - ")
-var question4 = ("<a> tegining vazifasi nima va qanday qilib havola yaratamiz? - ")
-var question5 = ("Rasm qo'shish uchun qaysi tegdan foydalanamiz? - ")
+// 1
+// let int = prompt("Son kiriting")
 
-var question6 = ("<ul> va <ol> teglarining farqi nima? - ")
-var question7 = ("CSS nima va u nima uchun ishlatiladi? - ")
-var question8 = ("CSSda rang berish uchun qanday xususiyatlardan foydalanamiz? - ")
-var question9 = ("margin va padding o'rtasidagi farq nima? - ")
-var question10 = ("CSSda matnni markazlashtirish uchun qaysi xususiyatdan foydalanamiz? - ")
-
-var question11 = ("CSS faylini HTMLga qanday ulash mumkin? - ")
-var question12 = ("JavaScript nima va u HTML bilan qanday ishlaydi? - ")
-var question13 = ("JavaScriptda o'zgaruvchilar qanday e'lon qilinadi? - ")
-var question14 = ("alert() funksiyasi nima uchun ishlatiladi? - ")
-var question15 = ("HTML elementini JavaScript orqali qanday tanlash mumkin? - ")
-
-var answer1 = prompt(question1)
-var answer2 = prompt(question2)
-var answer3 = prompt(question3)
-var answer4 = prompt(question4)
-var answer5 = prompt(question5)
-
-var answer6 = prompt(question6)
-var answer7 = prompt(question7)
-var answer8 = prompt(question8)
-var answer9 = prompt(question9)
-var answer10 = prompt(question10)
-
-var answer11 = prompt(question11)
-var answer12 = prompt(question12)
-var answer13 = prompt(question13)
-var answer14 = prompt(question14)
-var answer15 = prompt(question15)
-
-var a1 = ("HTML - veb-sahifa tuzilishini yaratadigan til bo‘lib, uning asosiy qismlari teglar va elementlardan iborat.")
-var a2 = ("HTML hujjati <!DOCTYPE html> bilan boshlanadi.")
-var a3 = ("Matnni qalin qilish uchun <b> yoki <strong> teglari ishlatiladi.")
-var a4 = ("<a> tegi havola yaratadi, masalan:</a>.")
-var a5 = ("Rasm qo'shish uchun <img> tegi ishlatiladi")
-
-var a6 = ("<ul> tartibsiz (belgili) ro'yxat yaratadi, <ol> esa tartibli (raqamli) ro'yxat yaratadi.")
-var a7= ("CSS - veb-sahifa uslubini yaratish uchun ishlatiladigan til; u sahifaning ko‘rinishi va dizaynini boshqaradi.")
-var a8 = ("color matnning rangini, background-color esa elementning fon rangini o'zgartiradi.")
-var a9 = ("margin elementning tashqi bo'sh joyini, padding esa ichki bo'sh joyini belgilaydi.")
-var a10 = ("Matnni markazlashtirish uchun text-align: center; xususiyati ishlatiladi.")
-
-var a11 = ("<link> tegidan foydalanib ulash mumkin.")
-var a12 = ("JavaScript - veb-sahifaga interaktivlik qo'shish uchun ishlatiladigan dasturlash tili bo‘lib, HTML bilan <script> tegida birikadi.")
-var a13 = ("var, let, yoki const kalit so'zlari bilan o'zgaruvchilar e'lon qilinadi, masalan, let x = 5;.")
-var a14 = ("alert() foydalanuvchiga xabar chiqarish uchun ishlatiladi.")
-var a15 = ("document.getElementById  yoki document.querySelector  kabi metodlardan foydalaniladi.")
+// if(int >= 1 && int <= 9){
+//     alert("1 xonali")
+// }
+// else if(int >= 10 && int <= 99){
+//     alert("2 xonali")
+// }
+// else if(int >= 100 && int <= 999){
+//     alert("3 xonali")
+// }
+// else if(int >= 1000 && int <= 9999){
+//     alert("4 xonali")
+// }
 
 
-console.log(question1,answer1,a1)
-console.log(question2,answer2,a2)
-console.log(question3,answer3,a3)
-console.log(question4,answer4,a4)
-console.log(question5,answer5,a5)
 
 
-console.log(question6,answer6,a6)
-console.log(question7,answer7,a7)
-console.log(question8,answer8,a8)
-console.log(question9,answer9,a9)
-console.log(question10,answer10,a10)
 
-console.log(question11,answer11,a11)
-console.log(question12,answer12,a12)
-console.log(question13,answer13,a13)
-console.log(question14,answer14,a14)
-console.log(question15,answer15,a15)
 
+
+
+// 2
+// let int = prompt("Son kiriting")
+
+// if( int% 2 == 0){
+//     alert("juft")
+// }                 
+// else{
+//         alert("toq")
+//     }
+
+
+
+
+
+// 3
+// let int = prompt("Son kiriting")
+// if( int% 2 == 0){                                           
+//     if(int >= 1 && int <= 9){
+//         alert("1 xonali")
+//     }
+//     else if(int >= 10 && int <= 99){
+//         alert("2 xonali")
+//     }
+//     else if(int >= 100 && int <= 999){
+//         alert("3 xonali")
+//     }
+//     else if(int >= 1000 && int <= 9999){
+//         alert("4 xonali")
+//     }
+//     alert("juft")
+// }
+// else{
+//     alert("toq")
+//     if(int >= 1 && int <= 9){
+//         alert("1 xonali")
+//     }
+//     else if(int >= 10 && int <= 99){
+//         alert("2 xonali")
+//     }
+//     else if(int >= 100 && int <= 999){
+//         alert("3 xonali")
+//     }
+//     else if(int >= 1000 && int <= 9999){
+//         alert("4 xonali")
+//     }
+// }
+
+
+
+
+
+
+
+
+
+//  4
+// let int = prompt("1 Son kiriting")
+// let int2 = prompt("2 Son kiriting")
+// let int3 = prompt("3 Son kiriting")
+
+// if(int>int2 && int>int3){
+//     if(int2>int3){
+//         console.log(nt,int2,int3)
+//     }
+//     else{
+//         console.log(nt,int3,int2)
+//     }
+// }
+
+// if(int2>int && int2>int3){
+//     if(int>int3){
+//        console.log(int2,int,int3)
+//     }
+//     else{
+//        console.log(int2,int3,int)
+//     }
+// }
+// if(int3>int && int3>int2){
+//     if(int>int2){
+//        console.log(int3,int,int2)
+//     }
+//     else{
+//        console.log(int3,int2,int)
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// 5
+// let int = prompt("3 xonali son kiriting")
+// let last = int%10
+// let first = Math.floor("int/100")
+// let fs = int/10
+// let sec = Math.floor(fs%10)
+// if(first>last  || first>sec){
+//     alert(first)
+// }
+// if(sec>first  || sec>last){
+//     alert(sec)
+// }
+// if(last>first  || last>sec){
+//     alert(last)
+// }
+
+
+
+
+
+
+
+
+
+
+
+// 6
+
+// let int = prompt("3 xonali son kiriting")-0
+// let a = int/10
+// let c = Math.floor(int/100)
+// let sec = Math.floor(a%10)
+// alert(sec) 
+
+
+
+
+
+
+
+
+// 7
+// let speed = prompt("Tezlikni kiriting (km/soat):")
+
+// let speedLimit = 70
+// let point = 5
+// if(speed<speedLimit){
+//     alert("Hammasi Yaxshi")
+// }
+// else{
+//     let point = Math.floor((speed-speedLimit)/5)
+//     if(point=>12){
+//         alert("Prava olindi")
+//     }
+//     else{
+//         alert(point)
+//     }
+// }
+
+
+
+
+
+
+
+// 8
+// alert("Togridan togri yosh sorash togri yaxshi emas,shuning uchun yilingizni ayting uzim topvolaman")
+// let year = prompt("Nechingchi yilsiz?")-0
+// let thisYear = 2024
+// let age = thisYear-year
+// alert(age + "- yoshdasiz")
+
+
+
+
+
+
+//9 
+// let int = prompt("Son kiriting")
+// let intNum = int.toString().length
+// alert("Sizning soningiz - "+ intNum +" xonali")
+
+// if(int%2 == 0){
+//     alert("Son Juft")
+// }
+// else(
+//     alert("Son Toq")
+// )
+// if(int <0){
+//     alert("Son Manfiy")
+// }
+// else{
+//     alert("Son Musbat")
+// }
+
+
+
+
+
+
+
+
+// 10
+// let int = prompt("Son kiriting")
+// let fizz
+// let buzz
+// if(int % 3 == 0 && int % 5 == 0){
+//     alert("Fizz & Buzz")
+// }
+// else if(int % 3 == 0){
+//     alert("Fizz")
+// }
+// else if(int % 5 == 0){
+//     alert("Buzz")
+// }
+
+
+
+
+
+
+
+
+
+
+
+//Special
+
+// let money = prompt("Alisher qancha puling bor?")
+// var dolar = 11000.34
+// var euro = 12354.03
+// var all = (750*dolar +120*euro)
+// if(money>=all){
+//     alert("Oq yol")
+// }
+// else{
+//     alert("Alisher, ozgina sabr qilish kerak bo'lar ekan")
+// }
